d80c0e736c052f43825d1c91898beaa58281d4e1
Irene <carina.omsk@gmail.com>
2017-01-17 02:27:09 +0000

    Started the function to fetch the list of contributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,17 @@
 var request = require('request');
+var https = require('https');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+  var request = require('request');
+  request('https://api.github.com/repos/jquery/jquery/contributors', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      cb(null, body);
+    }
+  })
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
