ff7f259a56859f7c3d8c76a00dfc510b7d2226c0
Irene <carina.omsk@gmail.com>
2017-01-17 03:44:20 +0000

    Made the proper request and printed avatar's url

--- download_avatars.js
+++ download_avatars.js
@@ -4,18 +4,29 @@ var https = require('https');
 var GITHUB_USER = "irene-webmaster";
 var GITHUB_TOKEN = "d7888ba3b192390894c72e3a6ec33dc2c6e28988";
 
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
 
-  request.get(requestURL)
-         .on('error', function (err) {
-           console.log(err);
-         })
-         .on('response', function (response) {
-           // console.log(response);
-           cb(null, response);
-         });
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request(options, function (error, response, body) {
+
+    if (!error && response.statusCode == 200) {
+      cb(null, JSON.parse(body));
+
+      var data = JSON.parse(body);
+      data.forEach(function(x){
+        console.log(x.avatar_url);
+      });
+    }
+  })
 }
 
 
