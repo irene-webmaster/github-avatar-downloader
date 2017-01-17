6253432cfda8e2e86285eee35ca581f5f1460685
Irene <carina.omsk@gmail.com>
2017-01-17 06:18:28 +0000

    Added support for command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,20 @@
 var request = require('request');
 var https = require('https');
 
+if (process.argv.length <= 3) {
+    console.log("Please enter GitHub repository owner and name");
+    process.exit(-1);
+}
+
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
+
 var GITHUB_USER = "irene-webmaster";
 var GITHUB_TOKEN = "d7888ba3b192390894c72e3a6ec33dc2c6e28988";
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors?per_page=100';
   console.log(requestURL);
 
   var options = {
@@ -60,7 +68,7 @@ function downloadImageByURL(url, filePath) {
 
 }
 
-getRepoContributors("nodejs", "node", function(err, result) {
+getRepoContributors(repoOwner, repoName, function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
